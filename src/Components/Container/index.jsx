9ef26120f5438c993a2abaae0b78a7323a2abe33
Janaina <janainasoul@icloud.com>
2023-05-11 15:57:07 -0300

    Refatoração

--- src/Components/Container/index.jsx
+++ src/Components/Container/index.jsx
@@ -1,17 +1,28 @@
 import React from "react";
-
+import styled from 'styled-components';
 import Titulo from "../Titulo";
 import Conta from "../Conta";
 
-const Container = () => {
-  return (
-    <div className="container">
-      <Titulo>Olá Byte Banker!</Titulo>
-      <section className="conteudo">
-        <Conta />
-      </section>
-    </div>
-  );
-};
+const Container = styled.div`
+  background-color: #f1f1f1;
+  min-height: 90vh;
+  padding: 0px 15vw;
+`;
+const Conteudo = styled.section`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
 
-export default Container;
+export default ()=> {
+  return (
+    <Container>
+      <Titulo>Olá Byte Banker!</Titulo>
+      <Conteudo >
+        <Conta />
+      </Conteudo>
+    </Container>
+  );
+  };
+
+//export default Container;
