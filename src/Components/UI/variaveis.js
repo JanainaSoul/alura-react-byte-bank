f910fb16290595e6b3dd64a126dfa4055c468ce9
Janaina <janainasoul@icloud.com>
2023-05-12 14:57:51 -0300

    Temas

--- src/Components/UI/variaveis.js
+++ src/Components/UI/variaveis.js
@@ -1,3 +1,9 @@
 export const corPrimaria = "#41d3be";
+
 export const fundoClaro = "#f1f1f1";
+export const conteudoClaro ="white";
 export const textoFundoClaro = "grey";
+
+export const conteudoEscuro ="#363537";
+export const fundoEscuro = "#5C5B5E";
+export const textoFundoEscuro = "#FAFAFA";
